33f3cea3fd2501f4709977d08e9817ed8862ef08
airbrn508 <chris.marquardt2@vzw.com>
2019-11-21 17:05:21 -0500

    End of day 2

---
+++ library/omnisci.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2019, Chris Marquardt <chris.marquardt2@vzw.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'marqch1'
+}
+
+DOCUMENTATION = '''
+---
+module: omnisci
+
+short_description: This is my test omnisci module
+
+version_added: "1.0"
+
+description:
+    - "This is my longer description explaining my test module"
+
+options:
+    platform:
+        description:
+            - This is the message to send to the test module
+        required: true
+    group:
+        description:
+            - Control to demo if the result of this module is changed or not
+        required: true
+    new:
+        description:
+            - Control to demo if the result of this module is changed or not
+        required: false
+
+extends_documentation_fragment:
+    - azure
+
+author:
+    - chris.marquardt2@vzw.com
+'''
+
+EXAMPLES = '''
+# Pass in a message
+- name: Test with a message
+  omnisci:
+    platform: spc
+    group: nrb
+
+# pass in a message and have changed true
+- name: Test with a message and changed output
+  omnisci:
+    platform: spc
+    group: nrb
+    new: true
+
+# fail the module
+- name: Test failure of the module
+  omnisci:
+    platform: spc
+    group: spg
+'''
+
+RETURN = '''
+original_message:
+    description: The original group param that was passed in
+    type: str
+    returned: always
+message:
+    description: The output message that the test module generates
+    type: str
+    returned: always
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        platform=dict(type='str', required=True),
+        group=dict(type='str', required=True),
+        new=dict(type='bool', required=False, default=False)
+    )
+
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # change is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False,
+        original_message='',
+        message=''
+    )
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        module.exit_json(**result)
+
+    # manipulate or modify the state as needed (this is going to be the
+    # part where your module will do what it needs to do)
+    result['original_message'] = module.params['platform']
+    result['message'] = 'goodbye'
+
+    result['original_message'] = module.params['group']
+    result['message'] = 'goodbye'
+
+    # use whatever logic you need to determine whether or not this module
+    # made any modifications to your target
+    if module.params['new']:
+        result['changed'] = True
+
+    # during the execution of the module, if there is an exception or a
+    # conditional state that effectively causes a failure, run
+    # AnsibleModule.fail_json() to pass in the message and the result
+    if module.params['platform'] != 'spc':
+        module.fail_json(msg='You requested this to fail', **result)
+
+    groups = ['devif','nrb','rdcs','wifi']
+    if module.params['group'] not in groups:
+        module.fail_json(msg='You requested this to fail', **result)
+
+    # in the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
+
+def main():
+    run_module()
+
+if __name__ == '__main__':
+    main()
+
